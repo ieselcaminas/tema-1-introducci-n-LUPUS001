c02e9ef14faafe7bc330085829cc159e44dec515
LUPUS23 <antoniodaw23@gmail.com>
2024-09-26 12:29:59 +0200

    w3resource first update

---
+++ src/w3resource/Ej1.java
@@ -0,0 +1,33 @@
+package w3resource;
+
+import java.util.Scanner;
+
+public class Ej1 {
+    public static void main(String[] args) {
+        int num1;
+        int num2;
+        int num3;
+        int num4;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese el primer numero");
+        num1 = sc.nextInt();
+
+        System.out.println("Ingrese el segundo numero");
+        num2 = sc.nextInt();
+
+        System.out.println("Ingrese el tercer numero");
+        num3 = sc.nextInt();
+
+        System.out.println("Ingrese el quarto numero");
+        num4 = sc.nextInt();
+
+        if (num1 == num2 && num2 == num3 && num3 == num4) {
+        //if (num1 - num2 == 0 && num2 - num3 == 0 && num3 - num4 == 0 ) {
+            System.out.println("EQUAL, los 4 números son iguales");
+        } else{
+            System.out.println("NOT EQUALS, no son iguales");
+        }
+
+    }
+}
