b5d3d34ff11c175ab0c956159fcd4e3e9acf663e
LUPUS23 <antoniodaw23@gmail.com>
2024-09-26 13:31:58 +0200

    w3resource update

---
+++ src/w3resource/Ej5_52.java
@@ -0,0 +1,30 @@
+package w3resource;
+
+import java.util.Scanner;
+
+public class Ej5_52 {
+    public static void main(String[] args) {
+        int num;
+        int num2;
+        int num3;
+        boolean siono;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese el primer numero");
+        num = sc.nextInt();
+        System.out.println("Ingrese el segundo numero");
+        num2 = sc.nextInt();
+        System.out.println("Finalmente ingrese el tercer numero");
+        num3 = sc.nextInt();
+
+        siono = num + num2 == num3;
+        System.out.println("Resultado es: " + siono);
+
+
+        /*if (num + num2 == num3){
+            System.out.println("El resultado es: correcto");
+        } else {
+            System.out.println("El resultado es: incorrecto");
+        }*/
+    }
+}
