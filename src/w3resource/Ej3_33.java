6a32f9ceac0fdf216c4b66d41e5f5b9b679c2052
LUPUS23 <antoniodaw23@gmail.com>
2024-09-26 13:03:22 +0200

    w3resource update

---
+++ src/w3resource/Ej3_33.java
@@ -0,0 +1,24 @@
+package w3resource;
+
+import java.util.Scanner;
+
+public class Ej3_33 {
+    public static void main(String[] args) {
+        int num;
+        int digito;
+        int suma = 0;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese un numero");
+        num = sc.nextInt();
+
+        do{
+            digito = num % 10;
+            suma += digito;
+            num = num / 10;
+
+        }while (num > 0);/*V1*/  //while (num > 10);/*V1*/
+        System.out.println(suma);/*V1*/
+        //System.out.println(suma + num);/*V2*/
+    }
+}
