0061db3fbfbb0ba8450d2552959984ce02234900
LUPUS23 <antoniodaw23@gmail.com>
2024-10-17 09:36:43 +0200

    Update

--- src/PrimerosPasos/Ejercicio24.java
+++ src/PrimerosPasos/Ejercicio24.java
@@ -4,29 +4,44 @@ import java.util.Scanner;
 
 public class Ejercicio24 {
     public static void main(String[] args) {
-        float nota;
-        float media = 0;
-        float suma = 0;
-        boolean esDiez = false;
-
-        System.out.println("INGRESE SUS NOTAS");
+        double num;
+        double media = 0;
+        boolean hayUnDiez = false;
+        int cantidad = 0;
         Scanner sc = new Scanner(System.in);
 
-        do {
-            nota = sc.nextInt();
-            if (nota != -1){
-                suma += nota;
-                media++;
-                if (nota == 10){
-                    esDiez = true;
-                }
-            }
+        System.out.println("**Añada TODAS sus notas para poder extraer la MEDIA**");
+        System.out.println("Introduzca -1 para terminar\n");
 
-        } while(nota != -1);
-        if (esDiez == true){
-            System.out.println("La nota media es " + String.format("%.1f",(suma / media)) + " y había un 10");
+        //Con esta versión al no poder un condicional -1 le restaria a la media y se sumaria a la cantidad por lo que pasaria de 15 / 3 a 14 / 4
+        /*do{
+           num = sc.nextDouble();
+           media += num;
+           cantidad++;
+           if (num == 10) {
+               hayUnDiez = true;
+           }
+        }while(num != -1);*/
+
+        do{
+            num = sc.nextDouble();
+            if (num != -1 ){ //y para no caer en el error mencionado hacemos este if
+                media += num;
+                cantidad++;
+            }
+            if (num == 10) {
+                hayUnDiez = true;
+            }
+        }while(num != -1);
+
+        if (cantidad > 0) {
+            System.out.printf("La nota media es %.1f%n", media / cantidad);
         } else {
-            System.out.println("La nota media es " + String.format("%.1f",(suma / media)) + " y NO había un 10");
+            System.out.println("No se introdujeron notas válidas.");
         }
+        if (hayUnDiez){
+            System.out.println("Hay un 10");
+        }
+
     }
 }
