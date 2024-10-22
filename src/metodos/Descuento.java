504d23d0aa22dc9f6f248b54b8db0e11d18f27ef
LUPUS23 <antoniodaw23@gmail.com>
2024-10-22 13:26:29 +0200

    Update

--- src/metodos/Descuento.java
+++ src/metodos/Descuento.java
@@ -11,11 +11,14 @@ public class Descuento {
     public static void main(String[] args) {
         double precio = Utilidades.leerEntero("Introduce el precio real");
         double precioRebajado = Utilidades.leerEntero("Introduce el precio rebajado");
-        double descuento;
 
-        descuento = descuentoN(precio, precioRebajado);
+        /*Version1*/
+        //double descuento = descuentoN(precio, precioRebajado);
 
-        System.out.println("El descuento es del " + descuento + "%");
+        //System.out.println("El descuento es del " + descuento + "%");
+
+        /*Version2*/
+        System.out.println("El descuento es del " + descuentoN(precio, precioRebajado) + "%");
     }
 }
 
