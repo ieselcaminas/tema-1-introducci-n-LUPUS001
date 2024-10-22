027b9d0d05cc320402967d2a29ef573b031e5c7b
LUPUS23 <antoniodaw23@gmail.com>
2024-10-22 13:35:12 +0200

    Update

--- src/metodos/diasMes.java
+++ src/metodos/diasMes.java
@@ -1,7 +1,42 @@
 package metodos;
 
 public class diasMes {
-    public static String diaMes(int mes){
+    public static int diaMes(int mes){
+        switch (mes){
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                return 31;
+            case 2:
+                return 28;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                return 30;
+            default:
+                return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int mes = Utilidades.leerEntero("Introduce el número del mes");
+        int dias;
+
+        dias = diaMes(mes);
+        if (dias == -1){
+            System.out.println("El mes no existe");
+        } else {
+            System.out.println("El mes tiene " + dias + " días");
+        }
+    }
+
+    //Otra opción de hacer el metodo
+    /*public static String diaMes(int mes){
         switch (mes){
             case 1:
             case 3:
@@ -13,14 +48,13 @@ public class diasMes {
                 return ("Tiene 31 días");
             case 2:
                 return ("Tiene 29 días");
-            default:
+            case 4:
+            case 6:
+            case 9:
+            case 11:
                 return ("Tiene 30 días");
+            default:
+                return ("El mes no existe");
         }
-    }
-
-    public static void main(String[] args) {
-        int mes = Utilidades.leerEntero("Introduce el número del mes");
-
-        System.out.println(diaMes(mes));
-    }
+    }*/
 }
